6dff09e4c68a87a4fa9120031579034a66bbf740
keith <keith@unitx.co.za>
2019-03-12 09:00:30 +0200

    CD

--- resources/views/pages/user/profile.blade.php
+++ resources/views/pages/user/profile.blade.php
@@ -49,61 +49,91 @@
         <div class="col-lg-8 col-xlg-9 col-md-7">
             <div class="card">
                 <div class="card-block">
-                    {!! Form::model($user, array('class'=>"form-horizontal form-material", 'action' => array('UsersManagementController@update', $user->id), 'method' => 'PUT')) !!}
+                    {!! Form::model($user, array('action' => array('UsersManagementController@update', $user->id), 'method' => 'PUT')) !!}
+
                         {!! csrf_field() !!}
-                        <div class="form-group">
-                            <label class="col-md-12">E mail</label>
-                            <div class="col-md-12">
-                                {!! Form::text('email', old('email'), array('id' => 'email', 'class' => 'form-control form-control-line', 'placeholder' => 'Email')) !!}
+
+                        <div class="form-group {{ $errors->has('email') ? ' has-error ' : '' }}">
+                            {!! Form::label('email', 'E-mail' , array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                                <div class="form-line">
+                                    {!! Form::text('email', old('email'), array('id' => 'email', 'class' => 'form-control form-control-line', 'placeholder' => trans('forms.ph-useremail'))) !!}
+                                </div>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="example-email" class="col-md-12">First Name</label>
-                            <div class="col-md-12">
-                              {!! Form::text('first_name', old('first_name'), array('id' => 'first_name', 'class' => 'form-control form-control-line', 'placeholder' => 'First Name')) !!}
+
+                        <div class="form-group {{ $errors->has('first_name') ? ' has-error ' : '' }}">
+                            {!! Form::label('first_name', trans('forms.create_user_label_firstname'), array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                            <div class="form-line">
+                                {!! Form::text('first_name', NULL, array('id' => 'first_name', 'class' => 'form-control form-control-line', 'placeholder' => trans('forms.create_user_ph_firstname'))) !!}
+                            </div>
+                            @if ($errors->has('first_name'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('first_name') }}</strong>
+                                </span>
+                            @endif
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="example-email" class="col-md-12">Last Name</label>
-                            <div class="col-md-12">
-                              {!! Form::text('last_name', old('last_name'), array('id' => 'last_name', 'class' => 'form-control form-control-line', 'placeholder' => 'Last Name')) !!}
+
+                        <div class="form-group {{ $errors->has('last_name') ? ' has-error ' : '' }}">
+                            {!! Form::label('last_name', trans('forms.create_user_label_lastname'), array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                            <div class="form-line">
+                                {!! Form::text('last_name', NULL, array('id' => 'last_name', 'class' => 'form-control form-control-line', 'placeholder' => trans('forms.create_user_ph_lastname'))) !!}
+                                
+                            </div>
+                            @if ($errors->has('last_name'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('last_name') }}</strong>
+                                </span>
+                            @endif
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label class="col-md-12">ID Number</label>
-                            <div class="col-md-12">
-                              {!! Form::text('id_number', old('id_number'), array('disabled'=>'', 'id' => 'id_number', 'class' => 'form-control form-control-line', 'placeholder' => 'ID Number')) !!}
+
+                        <div class="form-group {{ $errors->has('id_number') ? ' has-error ' : '' }}">
+                            {!! Form::label('id_number', 'ID Number', array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                            <div class="form-line">
+                                {!! Form::text('id_number', NULL, array('id' => 'id_number', 'class' => 'form-control form-control-line', 'placeholder' => 'ID Number')) !!}
+                            </div>
+                            @if ($errors->has('id_number'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('id_number') }}</strong>
+                                </span>
+                            @endif
                             </div>
                         </div>
-                        <!-- <div class="form-group">
-                            <label class="col-md-12">Phone No</label>
-                            <div class="col-md-12">
-                                <input type="text" placeholder="123 456 7890" class="form-control form-control-line">
+
+                        <div class="pw-change-container">
+                            <div class="form-group">
+                            {!! Form::label('password', trans('forms.create_user_label_password'), array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                                <div class="form-line">
+                                {!! Form::password('password', array('id' => 'password', 'class' => 'form-control ', 'placeholder' => trans('forms.create_user_ph_password'))) !!}
+                                </div>
+                            </div>
+                            </div>
+
+                            <div class="form-group">
+                            {!! Form::label('password_confirmation', trans('forms.create_user_label_pw_confirmation'), array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                                <div class="form-line">
+                                {!! Form::password('password_confirmation', array('id' => 'password_confirmation', 'class' => 'form-control form-control-line', 'placeholder' => trans('forms.create_user_ph_pw_confirmation'))) !!}
+                                
+                                </div>
+                            </div>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label class="col-md-12">Message</label>
-                            <div class="col-md-12">
-                                <textarea rows="5" class="form-control form-control-line"></textarea>
+                                            
+                        <div class="row">
+                            {!! Form::label('', '', array('class' => 'col-md-3 control-label')); !!}
+                            <div class="col-xs-12">
+                            {!! Form::button('<i class="fa fa-fw fa-save" aria-hidden="true"></i> Save Changes', array('class' => 'btn btn-success btn-block margin-bottom-1 btn-save','type' => 'button', 'data-toggle' => 'modal', 'data-target' => '#confirmSave', 'data-title' => trans('modals.edit_user__modal_text_confirm_title'), 'data-message' => trans('modals.edit_user__modal_text_confirm_message'))) !!}
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label class="col-sm-12">Select Country</label>
-                            <div class="col-sm-12">
-                                <select class="form-control form-control-line">
-                                    <option>London</option>
-                                    <option>India</option>
-                                    <option>Usa</option>
-                                    <option>Canada</option>
-                                    <option>Thailand</option>
-                                </select>
-                            </div>
-                        </div> -->
-                        <div class="form-group">
-                            <div class="col-sm-12">
-                                <button class="btn btn-success">Update Profile</button>
-                            </div>
-                        </div>
+                    </div>
+
                     {!! Form::close() !!}
                 </div>
             </div>
